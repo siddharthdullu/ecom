fa7ef3cf7446c33dd1409779a49e80ecb52d7d6b
SiddharthDullu <siddharth.dullu@gmail.com>
2020-10-04 11:35:27 +0530

    Initial Commit

---
+++ ecom/api/user/views.py
@@ -0,0 +1,168 @@
+# from rest_framework import viewsets
+# from rest_framework.permissions import AllowAny
+# from .serializers import UserSerializer
+# from .models import CustomUser
+# from django.http import JsonResponse
+# from django.contrib.auth import get_user_model
+# from django.views.decorators.csrf import csrf_exempt
+# from django.contrib.auth import login, logout
+# import random
+# import re
+
+# # Create your views here.
+
+# def generate_session_token(length=10):
+#     return ''.join(random.SystemRandom().choice([chr(i)for i in range(97,123)]+[str(i) for i in range(10)])for _ in range(10))
+
+# @csrf_exempt
+# def signin(request):
+#     if not request.method == 'POST':
+#         return JsonResponse({'error':'Send a POST req with valid parameters only'})
+
+
+#     username = request.POST['email']
+#     password = request.POST['password']
+
+#     if not re.match("([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}",username):
+#         return JsonResponse({'error':'Enter a valid email'})
+
+
+#     if len(password)<3:
+#         return JsonResponse({'error':'passwords needs to be of 3 character'})
+
+#     UserModel = get_user_model()    
+
+#     try:
+#         user = UserModel.objects.get(email=username)
+#         if user.check_password(password):
+#             usr_dict = UserModel.objects.filter(email=username).values().first()
+#             usr_dict.pop('password')
+#             if user.session_token !="0":
+#                 user.session_token = "0"
+#                 user.save()
+#                 return JsonResponse({'error':'previous session already exsists'})
+
+#             token = generate_session_token()
+#             user.session_token = token
+#             user.save()
+#             login(request,user)
+#             return JsonResponse({'token':token,'user':usr_dict})
+
+#         else:
+#             return JsonResponse({'error':'Invalid Password'})
+                         
+#     except UserModel.DoesNotExist:
+#         return JsonResponse({'error':'Invalid Email'})
+
+
+# def signout(request,id):
+    
+#     logout(request) #place
+
+#     UserModel = get_user_model()
+
+#     try:
+#         user = UserModel.objects.get(pk=id)
+#         user.session_token = "0"
+#         user.save()
+#     except UserModel.DoesNotExist:
+#         return JsonResponse({'error':'Invalid user Id'})
+    
+
+#     return JsonResponse({"success":"logout success"})
+
+
+
+# class UserViewSets(viewsets.ModelViewSet):
+#     permission_classes_by_action = {'create':[AllowAny]}
+
+#     queryset = CustomUser.objects.all().order_by('id')
+#     serializer_class= UserSerializer
+
+
+#     def get_permissions(self):
+#         try:
+#             return [permission() for permission in self.permission_classes_by_action[self.action]]
+#         except KeyError:
+#             return [permission() for permission in self.permission_classes]
+
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
+
+import random
+import re
+
+def generate_session_token(length=10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
+
+@csrf_exempt
+def signin(request):
+    if not request.method == 'POST':
+        return JsonResponse({'error':'Send a POST req with valid parameters only'})
+
+    username = request.POST['email']
+    password = request.POST['password']
+
+    if not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$",username):
+        return JsonResponse({'error':'Enter a valid email'})
+
+    if len(password) < 3:
+        return JsonResponse({'error':'passwords needs to be of 3 character'})
+
+    UserModel = get_user_model()    
+
+    try:
+        user = UserModel.objects.get(email=username)
+        
+        if user.check_password(password):
+            usr_dict = UserModel.objects.filter(email=username).values().first()
+            usr_dict.pop('password')
+            
+            if user.session_token !="0":
+                user.session_token = "0"
+                user.save()
+                return JsonResponse({'error':'previous session already exsists'})
+
+            token = generate_session_token()
+            user.session_token = token
+            user.save()
+            login(request,user)
+            return JsonResponse({'token': token,'user': usr_dict})
+        else:
+            return JsonResponse({'error':'Invalid Password'})
+                         
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error':'Invalid Email'})
+
+@csrf_exempt
+def signout(request,id):
+    
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        user.session_token = "0"
+        user.save()
+        logout(request)
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error':'Invalid user Id'})
+      
+    return JsonResponse({"success":"logout success"})
+
+class UserViewSets(viewsets.ModelViewSet):
+    permission_classes_by_action = {'create': [AllowAny]}
+
+    queryset = CustomUser.objects.all().order_by('id')
+    serializer_class= UserSerializer
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
---
+++ ecom/api/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+
+# Create your views here.
+
+
+def home(request):
+    return JsonResponse({'info': 'Django React Course','name':"Siddharth"})
+
